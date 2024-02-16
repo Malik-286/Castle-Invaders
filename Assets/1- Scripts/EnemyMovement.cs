2a6501808589ccf6da127afa8df9d6baf4c8c591
Malik Muhammad Arslan <malikarslan6162aus@gmail.com>
2024-02-17 03:02:34 +1100

    Enemy Movement Fixed

--- Assets/1- Scripts/EnemyMovement.cs
+++ Assets/1- Scripts/EnemyMovement.cs
@@ -4,22 +4,31 @@ using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour
 {
-    [SerializeField] float waitTime = 1.0f;
-    [SerializeField] List<WayPoint> path = new List<WayPoint>();
+     [SerializeField] List<WayPoint> path = new List<WayPoint>();
+     [SerializeField]  [Range(0,3)] float moveSpeed = 1f;
      void Start()
     {
 
-        StartCoroutine(FollowPath());
-       
+        StartCoroutine(FollowPath());      
     }
 
     IEnumerator FollowPath()
     {
         foreach(WayPoint waypoint in path)
         {
-            transform.position = waypoint.transform.position;
-            yield return new WaitForSeconds(waitTime);
-            Debug.Log(waypoint);
+            Vector3 startPosition = transform.position;
+            Vector3 endPosition = waypoint.transform.position;
+            float travelPercent = 0.0f;
+            transform.LookAt(endPosition);
+
+            while(travelPercent < 1f)
+            {
+                travelPercent += Time.deltaTime * moveSpeed;
+                transform.position = Vector3.Lerp(startPosition, endPosition, travelPercent);
+                yield return new WaitForEndOfFrame();
+            }
+
+           
         }
     }
 
