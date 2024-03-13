9f48f8996d81573b4476bbf5356c2ad94966b02b
Malik Muhammad Arslan <malikarslan6162aus@gmail.com>
2024-03-13 20:44:25 +1100

    Sound Effects Added

--- Assets/1- Scripts/AudioManager.cs
+++ Assets/1- Scripts/AudioManager.cs
@@ -4,10 +4,50 @@ using UnityEngine;
 
 public class AudioManager : Singleton<AudioManager>
 {
+
+    [HideInInspector]
+    AudioSource audioSource;
+    GameManager gameManager;
+
+    [SerializeField] AudioClip mainMenuSFX;
+    [SerializeField] AudioClip GamePlaySFX;
+
+
     protected override void Awake()
     {
         base.Awake();
+     }
+
+      void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        gameManager = FindObjectOfType<GameManager>();
+
     }
 
+    private void Update()
+    {
+        UpdateGamePlayAudio();  
+    }
+
+    public void PlaySingleShotAudio(AudioClip audioClip, float soundEffect)
+    {
+        audioSource.PlayOneShot(audioClip,soundEffect);
+    }
+
+
+    void  UpdateGamePlayAudio()
+    {
+        if(gameManager && audioSource != null)
+        {
+            if(gameManager.GetCurrentSceneName() == "Main Menu")
+            {
+                audioSource.clip = mainMenuSFX;
+            }else
+            {
+                audioSource.clip = GamePlaySFX;
+            }
+        }
+    }
   
 }
